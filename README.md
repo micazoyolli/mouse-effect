6d0b25b8e8fb246ab9a9844947017f0222666263
Nadia Aragon <nadia.aragon@the-cocktail.com>
2017-10-06 14:42:15 -0500

    feat(): Commit init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Mouse Effect
+Imagenes que se mueven con el movimiento del mouse.
+
+![Mouse Effect](https://github.com/micazoyolli/mouse-effect/blob/master/img/screenshot.png)
+
+##Demo
+---
+[Mouse Effect Demo](https://micazoyolli.github.io/mouse-effect/)
+
+## Animation
+---
+Puedes controlar que se muevan las imagenes horizontalmente, verticalmente o ambas con solo cambiar la propiedad `horizontal` `vertical` en el `main.js`
+
+```html
+var p = {
+  horizontal: true,
+  vertical: true
+};
+```
---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Properties
+var p = {
+  figures: document.querySelectorAll('#mouseEffect figure'),
+  horizontal: false,
+  mouseX: 0,
+  mouseY: 0,
+  vertical: false,
+  zone: document.querySelector('#mouseEffect')
+};
+
+// Methods
+var m = {
+  init: function() {
+    p.zone.addEventListener('mousemove', m.mouseMovement);
+
+    // Show images in tag figure
+    for (var i = 0; i < p.figures.length; i++) {
+      p.figures[i].innerHTML = '<img src="img/mouse/level0' + i + '.png">';
+
+      p.figures[i].style.zIndex = -i;
+    }
+
+    // After a second, get heigth of image in figure
+    setTimeout(function() {
+      p.zone.style.height = p.figures[0].childNodes[0].height + "px";
+    }, 100);
+
+  },
+
+  mouseMovement: function(mouse) {
+    p.mouseX = mouse.offsetX;
+    p.mouseY = mouse.offsetY;
+
+    for (var i = 0; i < p.figures.length; i++) {
+      if (p.horizontal) {
+        p.figures[i].style.left = -p.mouseX / (i * 100 + 150) + "%";
+      }
+
+      if (p.vertical) {
+        p.figures[i].style.top = -p.mouseY / (i * 100 + 150) + "%";
+      }
+
+    }
+  }
+};
+
+m.init();
